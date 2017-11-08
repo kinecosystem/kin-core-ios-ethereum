cf091f0e68c108be819441306604bb8d9e26ddbd
Ory Band <oryband@gmail.com>
2017-11-08 18:54:22 +0200

    rm obsolete truffle configuration

--- truffle.js
+++ truffle.js
@@ -7,18 +7,6 @@ module.exports = {
             host: 'localhost',
             port: 8545,
             network_id: '*' // Match any network id
-        },
-        coverage: {
-            host: "localhost",
-            network_id: "*",
-            port: 8555,
-            gas: 0xfffffffffff,
-            gasPrice: 0x01
         }
-    },
-    mocha: {
-        useColors: true,
-        slow: 30000,
-        bail: true
     }
 };
