77a150927e4c81f7d271758f043305684baee17d
Ory Band <oryband@gmail.com>
2017-11-08 18:49:19 +0200

    make a local copy of BasicTokenMock.sol
    truffle contract path is different in this repo than in kin-token

--- contracts/BasicTokenMock.sol
+++ contracts/BasicTokenMock.sol
@@ -1,1 +1,9 @@
-../kin-token/test/helpers/BasicTokenMock.sol
+pragma solidity ^0.4.15;
+
+import './BasicToken.sol';
+
+contract BasicTokenMock is BasicToken {
+    function assign(address _account, uint _balance) {
+        balances[_account] = _balance;
+    }
+}
