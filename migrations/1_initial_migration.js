19cdcf3ea5c94db3fd214a72f31ff885a5ee655f
Ory Band <oryband@gmail.com>
2017-11-08 18:50:29 +0200

    add truffle deploy (migration) script and contract
    - this sets up the testing environment
    - it assigns 1000 tokens to account[0]

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
-var Migrations = artifacts.require("./Migrations.sol");
+let Migrations = artifacts.require('./Migrations.sol');
+let BasicTokenMock = artifacts.require('../contracts/BasicTokenMock.sol');
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = (deployer, network, accounts) => {
+    deployer.deploy(Migrations);
+
+    deployer.deploy(BasicTokenMock).then(async () => {
+        let token = await BasicTokenMock.new();
+
+        token.assign(accounts[0], 1000);
+        console.log(`Assigned 1000 tokens to account ${accounts[0]} ...`);
+    });
 };
