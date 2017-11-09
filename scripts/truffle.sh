21ca09fd4495a263312ddd18102ec0adb3b94ed1
Ory Band <oryband@gmail.com>
2017-11-09 13:11:35 +0200

    add makefile with testrpc, truffle, tests support
    makes use of scripts/ dir:
      - runs testrpc and sets up 10 accounts
        with 100 ETH each, and one also with 1000 tokens
      - runs truffle and deploys token contract

---
+++ scripts/truffle.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+truffle deploy --reset > truffle.log 2>&1
+
+cat truffle.log | grep "Token contract deployed at" | tail -n 1 | awk '{print $5}' \
+    > token-contract-address
