65b06f756e4fe5602ee2a944540fe3957f1c623b
Natan Rolnik <natanrolnik@gmail.com>
2017-11-09 11:27:02 +0200

    KIK-7591 Creates sample app project

---
+++ KinSampleApp/KinSampleApp/Cells/KinClientCell.swift
@@ -0,0 +1,15 @@
+//
+//  KinClientCell.swift
+//  KinSampleApp
+//
+//  Created by Natan Rolnik on 07/11/2017.
+//  Copyright © 2017 Kin Foundation. All rights reserved.
+//
+
+import UIKit
+import KinSDK
+
+class KinClientCell: UITableViewCell {
+    var kinClient: KinClient!
+}
+
---
+++ KinSampleApp/KinSampleApp/KinSampleViewController.swift
@@ -0,0 +1,42 @@
+//
+//  KinSampleViewController.swift
+//  KinSampleApp
+//
+//  Created by Natan Rolnik on 06/11/2017.
+//  Copyright © 2017 Kin Foundation. All rights reserved.
+//
+
+import UIKit
+import KinSDK
+
+class KinSampleViewController: UITableViewController {
+    private var kinClient: KinClient!
+
+    class func instantiate(with kinClient: KinClient) -> KinSampleViewController {
+        guard let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "KinSampleViewController") as? KinSampleViewController else {
+            fatalError("Couldn't load KinSampleViewController from Main.storyboard")
+        }
+
+        viewController.kinClient = kinClient
+
+        return viewController
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+
+        if let kCell = cell as? KinClientCell {
+            kCell.kinClient = kinClient
+        }
+        
+        return cell
+    }
+
+
+}
