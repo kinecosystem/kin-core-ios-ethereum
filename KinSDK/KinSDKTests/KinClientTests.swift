410e1566f49cdac00e497f5de398eabc00690347
ashevin <avi.shevin@kik.com>
2017-11-15 07:52:08 +0200

    Convert kin<->wei at input and output.

--- KinSDK/KinSDKTests/KinClientTests.swift
+++ KinSDK/KinSDKTests/KinClientTests.swift
@@ -67,44 +67,4 @@ class KinClientTests: XCTestCase {
             XCTAssertTrue(false, "Something went wrong: \(error)")
         }
     }
-    
-    func test_kinToken() {
-
-        let kin100FromDouble = Decimal(100)
-        let bigIntShortString = GethNewBigInt(0)!
-        bigIntShortString.setString("100", base: 10)
-        let bigIntLongString = GethNewBigInt(0)!
-        bigIntLongString.setString("100000000000000000000", base: 10)
-        let bigIntLongStringReminder = GethNewBigInt(0)!
-        bigIntLongStringReminder.setString("100000000000000000001", base: 10)
-
-        guard let kin100FromUnder18CharsString = try? Decimal(kinString: "100") else {
-            XCTAssertTrue(false, "Kin could not be created from short string")
-            return
-        }
-        guard   let kin100FromOver18CharsString = try? Decimal(kinString: "100000000000000000000"),
-                let kin100FromOver18CharsStringWithReminder = try? Decimal(kinString: "100000000000000000001") else {
-            XCTAssertTrue(false, "Kin could not be created from long string")
-            return
-        }
-        guard   let KinFromBigIntWithInt = try? Decimal(bigInt: GethNewBigInt(100)),
-                let KinFromBigIntWithShortString = try? Decimal(bigInt: bigIntShortString),
-                let KinFromBigIntWithLongString = try? Decimal(bigInt: bigIntLongString),
-                let KinFromBigIntWithLongStringRemoinder = try? Decimal(bigInt: bigIntLongStringReminder) else {
-            XCTAssertTrue(false, "Kin could not be created from GethBigInt")
-            return
-        }
-
-        XCTAssertTrue((kin100FromUnder18CharsString as NSDecimalNumber).uint64Value == 100)
-        XCTAssertEqual(kin100FromUnder18CharsString, kin100FromOver18CharsString)
-        XCTAssertEqual(kin100FromDouble, kin100FromOver18CharsString)
-        XCTAssertTrue(String(describing: kin100FromOver18CharsString) == "100")
-        XCTAssertTrue(String(describing: kin100FromOver18CharsStringWithReminder) == "100.000000000000000001")
-        XCTAssertTrue(String(describing: KinFromBigIntWithLongStringRemoinder) == "100.000000000000000001")
-        XCTAssertNotEqual(kin100FromOver18CharsStringWithReminder, kin100FromOver18CharsString)
-        XCTAssertEqual(kin100FromDouble, KinFromBigIntWithLongString)
-        XCTAssertEqual(kin100FromDouble, KinFromBigIntWithShortString)
-        XCTAssertEqual(kin100FromDouble, KinFromBigIntWithInt)
-
-    }
 }
